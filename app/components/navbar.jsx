a3cbc601d84055c0e6804e867d7cc8cdd60de96c
Swapneel Chaudhuri <113018040+swapneel111@users.noreply.github.com>
2024-07-22 17:53:37 +0000

    unchange

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,39 +1,22 @@
 // @flow strict
 "use client"; // This line makes the component a Client Component
 
-import { useState } from "react";
 import Link from "next/link";
-import { MenuIcon, XIcon } from "@heroicons/react/outline"; // Icons for mobile menu
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <nav className="bg-transparent">
       <div className="flex items-center justify-between py-5">
         <div className="flex flex-shrink-0 items-center">
-          <Link href="/" className="text-base font-bold text-gradient">
+          <Link
+            href="/"
+            className="text-base font-bold text-gradient" // Gradient for the name
+          >
             SWAPNEEL CHAUDHURI
           </Link>
         </div>
-        <div className="block md:hidden">
-          <button onClick={toggleMenu} className="focus:outline-none">
-            {isOpen ? (
-              <XIcon className="h-6 w-6 text-pink-600" />
-            ) : (
-              <MenuIcon className="h-6 w-6 text-pink-600" />
-            )}
-          </button>
-        </div>
-        <ul
-          className={`${
-            isOpen ? "block" : "hidden"
-          } mt-4 flex w-full flex-col space-y-2 text-base font-bold md:mt-0 md:flex md:flex-row md:space-x-4 md:space-y-0`}
-        >
+
+        <ul className="mt-4 flex w-full justify-center space-x-4 text-base font-bold md:mt-0 md:flex-row md:space-x-1">
           {["ABOUT", "EXPERIENCE", "SKILLS", "EDUCATION", "BLOGS", "PROJECTS"].map((item) => (
             <li key={item}>
               <Link
